3c6de930ecb280a5d1e2070e3ce35c86429dfd62
Prince Gajjar <princegajjar8002@gmail.com>
2026-05-09 13:52:21 +0530

    fix: remove invalid manualChunks configuration preventing vercel build

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'framer-motion'],
-        },
-      },
-    },
-  },
+
 });
